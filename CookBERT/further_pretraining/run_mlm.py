cc97036973e8fa00cc77ca616500a6dc9d1f0265
Pascal.Strobel <53040413+paschistrobel@users.noreply.github.com>
2022-03-27 22:32:00 +0200

    Final - 1

--- CookBERT/further_pretraining/run_mlm.py
+++ CookBERT/further_pretraining/run_mlm.py
@@ -37,33 +37,27 @@ from transformers import (
     AutoConfig,
     AutoModelForMaskedLM,
     AutoTokenizer,
+    BertTokenizerFast, # tokenizer used for CookBERT
     DataCollatorForLanguageModeling,
     HfArgumentParser,
     Trainer,
     TrainingArguments,
     set_seed,
-    BertTokenizerFast, # von mir hinzugefügt; FoodBERT verwendet normalen BertTokenizer
 )
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
 
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.15.0") # original version: "4.16.0.dev0"
+check_min_version("4.15.0")
 
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/language-modeling/requirements.txt")
 
 logger = logging.getLogger(__name__)
-# list of model config classes:
-# [<class 'transformers.models.qdqbert.configuration_qdqbert.QDQBertConfig'>, <class 'transformers.models.fnet.configuration_fnet.FNetConfig'>, ..]
 MODEL_CONFIG_CLASSES = list(MODEL_FOR_MASKED_LM_MAPPING.keys())
-# tuple of model names:
-# ('qdqbert', 'fnet', 'perceiver', 'rembert', 'roformer', 'big_bird', 'wav2vec2', 'convbert', ...)
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
 
 @dataclass
 class ModelArguments:
-    # benötigt: Checkpoint for weight initialization
     model_name_or_path: Optional[str] = field(
         default=None,
         metadata={
@@ -71,12 +65,10 @@ class ModelArguments:
             "Don't set if you want to train a model from scratch."
         },
     )
-    # nicht benötigt
     model_type: Optional[str] = field(
         default=None,
         metadata={"help": "If training from scratch, pass a model type from the list: " + ", ".join(MODEL_TYPES)},
     )
-    # nicht benötigt
     config_overrides: Optional[str] = field(
         default=None,
         metadata={
@@ -84,30 +76,24 @@ class ModelArguments:
             "n_embd=10,resid_pdrop=0.2,scale_attn_weights=false,summary_type=cls_index"
         },
     )
-    # nicht benötigt
     config_name: Optional[str] = field(
         default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
     )
-    # unsicher, da eigener Tokenizer verwendet wird
     tokenizer_name: Optional[str] = field(
         default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
     )
-    # unsicher
     cache_dir: Optional[str] = field(
         default=None,
         metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
     )
-    # FastTokenizer verwenden! Default ist allerdings eh schon True
     use_fast_tokenizer: bool = field(
         default=True,
         metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
     )
-    # unsicher, aber wahrscheinlich auf default Wert belassen
     model_revision: str = field(
         default="main",
         metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
     )
-    # unsicher
     use_auth_token: bool = field(
         default=False,
         metadata={
@@ -122,39 +108,29 @@ class ModelArguments:
                 "--config_overrides can't be used in combination with --config_name or --model_name_or_path"
             )
 
-# Argumente, die sich auf die DATEN für Training und Evaluation beziehen
 @dataclass
 class DataTrainingArguments:
-    """
-    Arguments pertaining to what data we are going to input our model for training and eval.
-    """
-
-    # nicht benötigt, da ich kein Datensatz der Library verwende
+    
     dataset_name: Optional[str] = field(
         default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
     )
-    # nicht benötigt, da ich keinen Datensatz der Library verwende
     dataset_config_name: Optional[str] = field(
         default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
     )
-    # benötigt: recipeNLG instructions als text file
     train_file: Optional[str] = field(default=None, metadata={"help": "The input training data file (a text file)."})
     validation_file: Optional[str] = field(
         default=None,
         metadata={"help": "An optional input evaluation data file to evaluate the perplexity on (a text file)."},
     )
-    # unsicher, wahrschl auf False belassen
     overwrite_cache: bool = field(
         default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
     )
-    # wahrschl. benötigt, da kein validation set vorhanden
     validation_split_percentage: Optional[int] = field(
         default=5,
         metadata={
             "help": "The percentage of the train set used as validation set in case there's no validation split"
         },
     )
-    # unsicher
     max_seq_length: Optional[int] = field(
         default=None,
         metadata={
@@ -162,21 +138,17 @@ class DataTrainingArguments:
             "than this will be truncated."
         },
     )
-    # unsicher
     preprocessing_num_workers: Optional[int] = field(
         default=None,
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
-    # auf default lassen (bzw. erst überprüfen, ob ursprünglicher BERT auch mit 0.15 trainiert wurde)
     mlm_probability: float = field(
         default=0.15, metadata={"help": "Ratio of tokens to mask for masked language modeling loss"}
     )
-    # unsicher, da es schon Sinn machen würde, da jede Zeile einem Rezept entspricht
     line_by_line: bool = field(
         default=False,
         metadata={"help": "Whether distinct lines of text in the dataset are to be handled as distinct sequences."},
     )
-    # unsicher, aber wahrscheinlich afu False belassen
     pad_to_max_length: bool = field(
         default=False,
         metadata={
@@ -184,7 +156,6 @@ class DataTrainingArguments:
             "If False, will pad the samples dynamically when batching to the maximum length in the batch."
         },
     )
-    # nicht benötigt
     max_train_samples: Optional[int] = field(
         default=None,
         metadata={
@@ -192,7 +163,6 @@ class DataTrainingArguments:
             "value if set."
         },
     )
-    # nicht benötigt
     max_eval_samples: Optional[int] = field(
         default=None,
         metadata={
@@ -217,9 +187,8 @@ class DataTrainingArguments:
 
 def main():
     # See all possible arguments in src/transformers/training_args.py
-    # --> https://github.com/huggingface/transformers/blob/master/src/transformers/training_args.py
+    # --> https://github.com/huggingface/transformers/blob/master/src/transformers/training_args.py (Retrieved at: 06.01.2022)
     # or by passing the --help flag to this script.
-    # We now keep distinct sets of args, for a cleaner separation of concerns.
 
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
@@ -251,7 +220,6 @@ def main():
     # Set the verbosity to info of the Transformers logger (on main process only):
     logger.info(f"Training/evaluation parameters {training_args}")
 
-    ## Nochmal genauer anschauen ##################################################################
     # Detecting last checkpoint.
     last_checkpoint = None
     if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
@@ -268,16 +236,17 @@ def main():
             )
 
     # Set seed before initializing model.
-    set_seed(training_args.seed)  # necessary for full reproducability of the model
+    set_seed(training_args.seed)
 
     ############################################################################
     # Load datasets for training/ validation
     ############################################################################
+    
     # You can either
     # - provide your own CSV/JSON/TXT training and evaluation files (see below)
     #   --> For CSV/JSON files, this script will use the column called 'text' or the first column.
     #       You can easily tweak this behavior (see below)
-    #   --> See more about loading custom data: https://huggingface.co/docs/datasets/loading_datasets.html
+    #   --> See more about loading custom data: https://huggingface.co/docs/datasets/loading_datasets.html (Retrieved at: 06.01.2022)
     # - provide the name of a public datasets available on the hub at https://huggingface.co/datasets/ (will be downloaded automatically)
     if data_args.dataset_name is not None:  # name of public dataset was provided
         # Downloading and loading a dataset from the hub.
@@ -304,16 +273,15 @@ def main():
             # train(data) file was specified
             data_files["train"] = data_args.train_file
             extension = data_args.train_file.split(".")[-1]  # extract file extension ("txt", "json" or "csv")
-        if data_args.validation_file is not None:
-            # validation(data) file was specified
+        if data_args.validation_file is not None: # validation(data) file was specified
             data_files["validation"] = data_args.validation_file
             extension = data_args.validation_file.split(".")[-1]
         if extension == "txt":
             extension = "text"
         raw_datasets = load_dataset(extension, data_files=data_files, cache_dir=model_args.cache_dir)
 
-        # If no validation data is there, validation_split_percentage will be used to divide the dataset.
-        if "validation" not in raw_datasets.keys():
+        
+        if "validation" not in raw_datasets.keys(): # no validation data specified --> validation_split_percentage will be used to divide the dataset
             raw_datasets["validation"] = load_dataset(
                 extension,
                 data_files=data_files,
@@ -338,47 +306,52 @@ def main():
         "revision": model_args.model_revision,
         "use_auth_token": True if model_args.use_auth_token else None,
     }
-    if model_args.config_name:  # sollte nicht spezifiziert sein (also None) und somit nicht ausgeführt werden
+    if model_args.config_name:
         config = AutoConfig.from_pretrained(model_args.config_name, **config_kwargs)
-    elif model_args.model_name_or_path:  # sollte "bert-base-cased" sein
+    elif model_args.model_name_or_path:  # CookBERT uses this argument to either pass in "bert-base-cased" or a training checkpoint
         config = AutoConfig.from_pretrained(model_args.model_name_or_path, **config_kwargs)
-    else:  # wird ausgeführt falls kein model_name oder config_name angegeben wurde; neue config Instanz von Scratch erzeugen
+    else:
         config = CONFIG_MAPPING[model_args.model_type]()
         logger.warning("You are instantiating a new config instance from scratch.")
         if model_args.config_overrides is not None:
             logger.info(f"Overriding config: {model_args.config_overrides}")
             config.update_from_string(model_args.config_overrides)
             logger.info(f"New config: {config}")
-
-    # Load tokenizer
-    # Hier muss evtl. was geändert werden, da eigener Tokenizer mit erweitertem Vokabular verwendet wird
-    '''
+    
     tokenizer_kwargs = {
         "cache_dir": model_args.cache_dir,
-        "use_fast": model_args.use_fast_tokenizer,
+        "use_fast": model_args.use_fast_tokenizer, # default: True
         "revision": model_args.model_revision,
         "use_auth_token": True if model_args.use_auth_token else None,
     }
     if model_args.tokenizer_name:
         tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
-    elif model_args.model_name_or_path:
-        tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, **tokenizer_kwargs)
+    elif model_args.model_name_or_path: 
+        # create fast tokenizer
+        tokenizer = BertTokenizerFast.from_pretrained(model_args.model_name_or_path, **tokenizer_kwargs)
+        # check if model was loaded from checkpoint or is "newly" created
+        if bool("checkpoint" not in model_args.model_name_or_path):
+            logger.info("Tokenizer loaded from official BERT checkpoint. Additional vocabulary will be added...")
+            # add tokens to tokenizers vocabulary; see https://github.com/huggingface/transformers/issues/1413 (Retrieved at: 06.01.2022)
+            with open('CookBERT/further_pretraining/vocabulary/vocab_to_add.txt') as f:
+                vocabs_to_add = [line.rstrip() for line in f]
+            num_of_added_tokens = tokenizer.add_tokens(vocabs_to_add)
+            logger.info(f"{num_of_added_tokens} were successfully added to the vocabulary!")
+            logger.info(f"Vocab-size before: {len(tokenizer) - num_of_added_tokens}")
+            logger.info(f"Vocab-size after: {len(tokenizer)}")
+        else:
+            logger.info("Tokenizer loaded from own checkpoint. No additional vocabulary will be added (since it was already done)!")
     else:
         raise ValueError(
             "You are instantiating a new tokenizer from scratch. This is not supported by this script."
             "You can do it from another script, save it, and load it from here, using --tokenizer_name."
         )
-    '''
-
-    ### Hier eigenen Tokenizer verwenden
-    tokenizer = BertTokenizerFast(vocab_file="link_to_adapted_vocab", do_lower_case=False, max_len=128)
-
+    
     # Load model
     if model_args.model_name_or_path:
         model = AutoModelForMaskedLM.from_pretrained(
             model_args.model_name_or_path,
             from_tf=bool(".ckpt" in model_args.model_name_or_path),
-            # True when path to tensorflow index checkpoint file (e.g, ./tf_model/model.ckpt.index) was provided in model_name_or_path
             config=config,
             cache_dir=model_args.cache_dir,
             revision=model_args.model_revision,
@@ -394,6 +367,7 @@ def main():
     ############################################################################
     # Preprocessing the datasets
     ############################################################################
+    
     # First we tokenize all the texts.
     if training_args.do_train:
         column_names = raw_datasets["train"].column_names
@@ -401,7 +375,7 @@ def main():
         column_names = raw_datasets["validation"].column_names
     text_column_name = "text" if "text" in column_names else column_names[0]
 
-    if data_args.max_seq_length is None:  # Falls keine max_seq_length als Arg übergeben, dann wird die seq_length vom Tokenizer verwendet
+    if data_args.max_seq_length is None:  # if max_seq_length not defined in arguments, seq_length from tokenizer is used
         max_seq_length = tokenizer.model_max_length
         if max_seq_length > 1024:
             logger.warning(
@@ -409,7 +383,7 @@ def main():
                 "Picking 1024 instead. You can change that default value by passing --max_seq_length xxx."
             )
             max_seq_length = 1024
-    else:  # falls max_seq_length in Arg angegeben und != der tokenizer seq_length, dann einfach den kleineren der beiden Werte verwenden
+    else:  # if max_seq_length defined in arguments, but max_seq_length != seq_length of tokenizer, then the smaller value is used
         if data_args.max_seq_length > tokenizer.model_max_length:
             logger.warning(
                 f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the"
@@ -417,7 +391,7 @@ def main():
             )
         max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
 
-    if data_args.line_by_line:  # line_by_line Argument = True --> (Whether distinct lines of text in the dataset are to be handled as distinct sequences.)
+    if data_args.line_by_line:
         # When using line_by_line, we just tokenize each nonempty line.
         padding = "max_length" if data_args.pad_to_max_length else False
 
@@ -532,8 +506,11 @@ def main():
     if training_args.do_train:
         checkpoint = None
         if training_args.resume_from_checkpoint is not None:
+            print("\nResume from checkpoint specified in 'resume_from_checkpoint'\n")
             checkpoint = training_args.resume_from_checkpoint
         elif last_checkpoint is not None:
+            print("\nResume from last_checkpoint")
+            print(last_checkpoint, end="\n")
             checkpoint = last_checkpoint
         train_result = trainer.train(resume_from_checkpoint=checkpoint)
         trainer.save_model()  # Saves the tokenizer too for easy upload
